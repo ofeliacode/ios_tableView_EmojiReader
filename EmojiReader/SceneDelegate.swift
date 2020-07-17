ba84c6549323c63ea7b8beac5db64fbd0ef3b9fb
ofeliacode <56733536+ofeliacode@users.noreply.github.com>
2020-07-17 14:40:56 +0300

    Update SceneDelegate.swift

--- EmojiReader/SceneDelegate.swift
+++ EmojiReader/SceneDelegate.swift
@@ -1,10 +1,4 @@
-//
-//  SceneDelegate.swift
-//  EmojiReader
-//
-//  Created by Алексей Пархоменко on 09.03.2020.
-//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
-//
+
 
 import UIKit
 
